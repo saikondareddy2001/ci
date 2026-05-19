d4291d2efcc8363fcb8c64de844fdd9a12c3f7ea
saikondareddy2001 <158286899+saikondareddy2001@users.noreply.github.com>
2026-05-19 22:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello CI/CD! Version 1 is live!"
+    return "Hello CI/CD! Version 1.23 is live!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
